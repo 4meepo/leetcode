d55eebaf5567baae42f053ff30d5fb57918b3cdf
Yifei Liu <yifeiliu@tgmgrp.com>
2022-12-02 17:58:02 +0800

    [300] 最长递增子序列

---
+++ 300.最长递增子序列.go
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=300 lang=golang
+ *
+ * [300] 最长递增子序列
+ */
+package main
+
+// @lc code=start
+func lengthOfLIS(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	dp := make([]int, len(nums))
+
+	rtn := 1
+	for i, v := range nums {
+		if i == 0 {
+			dp[0] = 1
+			continue
+		}
+
+		max := 1
+		for j := 0; j < i; j++ {
+			if nums[j] < v {
+				if dp[j]+1 > max {
+					max = dp[j] + 1
+				}
+			}
+		}
+		dp[i] = max
+		if max > rtn {
+			rtn = max
+		}
+	}
+
+	return rtn
+}
+
+// @lc code=end
