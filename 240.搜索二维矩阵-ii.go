f028232a43177f8ad6b4d67c8eca2213e0820ab1
Yifei Liu <yifeiliu@tgmgrp.com>
2022-12-02 16:13:48 +0800

    240 optimize

--- 240.搜索二维矩阵-ii.go
+++ 240.搜索二维矩阵-ii.go
@@ -5,30 +5,22 @@
  */
 package main
 
-import "sort"
-
 // @lc code=start
+// z字形搜索
+// https://leetcode.cn/problems/search-a-2d-matrix-ii/solutions/1062538/sou-suo-er-wei-ju-zhen-ii-by-leetcode-so-9hcx/
 func searchMatrix(matrix [][]int, target int) bool {
 	m, n := len(matrix), len(matrix[0])
-	for x, y := n-1, 0; x >= 0 && y < m; x, y = x-1, y+1 {
-		cur := matrix[y][x]
-		if target == cur {
+
+	for x, y := n-1, 0; x >= 0 && y < m; {
+		num := matrix[y][x]
+		if num == target {
 			return true
 		}
-		if target < cur {
-			index := sort.SearchInts(matrix[y], target)
-			if index >= x {
-				// not found
-			} else if matrix[y][index] == target {
-				return true
-			}
-			// not found
-		} else if target > cur {
-			for _y := y; _y < m; _y++ {
-				if matrix[_y][x] == target {
-					return true
-				}
-			}
+
+		if num < target {
+			y++
+		} else if num > target {
+			x--
 		}
 	}
 
