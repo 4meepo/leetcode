651e9fab6c2bddbf42c92982715ef3aee5bc2751
Yifei <workeryifei@outlook.com>
2022-12-14 23:42:50 +0800

    98

---
+++ 98.验证二叉搜索树.go
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode.cn id=98 lang=golang
+ *
+ * [98] 验证二叉搜索树
+ */
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// @lc code=start
+/**
+* Definition for a binary tree node.
+ type TreeNode struct {
+    Val int
+    Left *TreeNode
+    Right *TreeNode
+}
+*/
+
+func isValidBST(root *TreeNode) bool {
+	var data []int
+	return isValidBST_middleTraverse(root, &data)
+}
+
+// 利用中序递归遍历判断
+func isValidBST_middleTraverse(root *TreeNode, data *[]int) bool {
+	if root == nil {
+		return true
+	}
+
+	if b := isValidBST_middleTraverse(root.Left, data); b == false {
+		return false
+	}
+
+	d := *data
+	if len(d) > 0 {
+		if root.Val <= d[len(d)-1] {
+			return false
+		}
+	}
+	*data = append(*data, root.Val)
+
+	if b := isValidBST_middleTraverse(root.Right, data); b == false {
+		return false
+	}
+	return true
+}
+
+// @lc code=end
